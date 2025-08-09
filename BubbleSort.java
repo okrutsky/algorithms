bcbf73e28fdb0985955aebe9f023bc3a8e8d617f
Ondrej Krutsky <ondrej@krutsky.com>
2025-08-09 22:45:40 +0200

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,26 @@
+package Algorithms;
+
+public class BubbleSort {
+	
+	public static void main(String[]args) {
+		
+	int [] arr = {5,2,10,7,4,3};
+	
+	for (int i=0; i <arr.length -1; i++){
+		for(int j=0; j <arr.length -1 -i; j++) {
+			
+			if(arr[j] > arr[j+1]) {
+				int tmp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = tmp;
+			}
+		}
+	}
+	for (int n: arr) {
+		System.out.println(n + "");
+	}
+	
+	}
+}
+	
+	
